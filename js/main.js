f4261c68671aac07afb0813996a1e4f051a6c003
jiikko <n905i.1214@gmail.com>
2015-11-01 16:39:06 +0900

    👽

--- js/main.js
+++ js/main.js
@@ -3,16 +3,18 @@ function fire() {
 
   var textarea = $("#input");
   var showcase = $("#showcase");
+  var now = $("#now");
   var segs = segmenter.segment(textarea.val());
   segs.push("f");
 
-  var time = 0
+  var time = 0;
   $.each(segs, function(i, seg){
-    time = i * 100
-      console.log(time);
-    setTimeout(function(){
-      showcase.text(seg);
-//      console.log(seg);
-    }, time);
+    time = (i * 100);
+    setTimeout(function(current_time){
+      return function() {
+        showcase.text(seg);
+        now.css("width", current_time / time * 100 + '%')
+      }
+    }(time), time);
   });
 }
