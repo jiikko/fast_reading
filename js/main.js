752b9f083025b541349ece81ef3c6ce127b4900e
jiikko <n905i.1214@gmail.com>
2015-11-01 23:40:44 +0900

    時間です

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function fire() {
+  var startTime = new Date();
   var segmenter = new TinySegmenter();
-
   var textarea = $("#input");
   var showcase = $("#showcase");
   var now = $("#now");
   var segs = segmenter.segment(textarea.val());
-  segs.push("f");
+  segs.push(null);
 
   var time = 0;
   $.each(segs, function(i, seg){
@@ -14,6 +14,11 @@ function fire() {
       return function() {
         showcase.text(seg);
         now.css("width", current_time / time * 100 + '%')
+        if(seg === null) {
+          var endTime = new Date();
+          $("#time").text((endTime - startTime)/1000 + '秒');
+          showcase.text("糸冬わり");
+        }
       }
     }(time), time);
   });
